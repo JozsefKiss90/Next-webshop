8c089823b878e00c05d296c42fd73514344d3627
silentstorm902 <jozsefkiss90@gmail.com>
2023-06-28 15:06:20 +0200

    removed stripe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,38 +2,16 @@ import { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css'; 
 import '../styles/global.css';
 import '../components/pageComponents/carousel.css';
-import {loadStripe} from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
 import GlobalState from "../context/GlobalState"
-import {useState,useEffect} from "react";
-import { useRouter } from "next/router";
 
 const MyApp = ({ Component, pageProps } : AppProps) => {
 
- /* useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);*/
-
-  const[publishabkeKey, setPublishabkeKey] = useState<string>("")
-
-  useEffect(() => {
-    fetch('api/keys', {
-      method:'GET',
-      headers: {'Content-Type':'application/json'}
-    })
-    .then((res)=>res.json())
-    .then((data)=>{
-      setPublishabkeKey(data.publishabkeKey)
-      })
-  }, []);
-  console.log(publishabkeKey)
-  const stripe = loadStripe(publishabkeKey)
 
   return (
     <GlobalState>
-      <Elements stripe={stripe}>
+     
         <Component {...pageProps} />
-      </Elements>
+  
     </GlobalState>
   )
 }
